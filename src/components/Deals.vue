50afc9f7fc02a97b7e40eeaf908947677d89591f
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-03-01 22:39:53 +0500

    deals section stylize almost finished

--- src/components/Deals.vue
+++ src/components/Deals.vue
@@ -20,12 +20,21 @@
                 </div>
 
                 <div class="car-descr">
-                    <span class="car-engine descr-txt">{{ car.engine }} L</span>
-                    <span class="car-drivetrain descr-txt">{{ car.drivetrain }}</span>
-                    <span class="car-body descr-txt">{{ car.body }}</span>
+                    <span class="car-engine descr-txt">
+                        <img :src="engineIcon" alt="" class="car-descr-icon">
+                        {{ car.engine }} L
+                    </span>
+                    <span class="car-drivetrain descr-txt">
+                        <img :src="drivetrainIcon" alt="" class="car-descr-icon">
+                        {{ car.drivetrain }}
+                    </span>
+                    <span class="car-body descr-txt">
+                        <img :src="bodyIcon" alt="" class="car-descr-icon">
+                        {{ car.body }}
+                    </span>
                 </div>
 
-                <button class="car-card-btn">{{ car.price }} {{ car.currency }}</button>
+                <button class="car-card-btn">{{ splitPrice(car.price) }} {{ car.currency }}</button>
             </div>
         </div>
     </div>
@@ -39,7 +48,26 @@ export default {
     name: 'Deals Section',
     data() {
         return {
-            store: carsStore()
+            store: carsStore(),
+            engineIcon: 'https://firebasestorage.googleapis.com/v0/b/union-auto-trade.appspot.com/o/Cars%2Ficons%2Fengine.svg?alt=media&token=cd93f535-407f-4b56-8321-3021b2d70cfb',
+            drivetrainIcon: 'https://firebasestorage.googleapis.com/v0/b/union-auto-trade.appspot.com/o/Cars%2Ficons%2Fdrivetrain.svg?alt=media&token=e0d1df14-1a44-4605-ba87-d462ac54ce4a',
+            bodyIcon: 'https://firebasestorage.googleapis.com/v0/b/union-auto-trade.appspot.com/o/Cars%2Ficons%2Fbody.svg?alt=media&token=16a8c95c-1122-4cce-8342-4045b992a275'
+        }
+    },
+    methods: {
+        splitPrice(price) {
+            price = price.toString().split('');
+            const insertIndices = [3, 2, 1, 2, 3];
+            const length = price.length;
+
+            if (length >= 5 && length <= 9) {
+                price.splice(insertIndices[length - 2], 0, ' ');
+                if (length >= 7) {
+                    price.splice(5, 0, ' ');
+                }
+            }
+
+            return price.join('');
         }
     }
 }
@@ -60,13 +88,99 @@ export default {
 
     .car-card {
         width: 100%;
-        border: solid 2px #D9D9D9;
+        border: solid 1px #D9D9D9;
         border-radius: 10px;
-        padding: 20px;
+        padding: 20px 15px;
         display: flex;
         flex-direction: column;
         row-gap: 10px;
         justify-content: space-between;
+        cursor: pointer;
+        background: var(--bg-color);
+        transition: .3s;
+
+        &-top {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            gap: 8px;
+
+            .car-model {
+                max-width: 230px;
+                width: 100%;
+                display: flex;
+                flex-direction: column;
+                row-gap: 5px;
+                font-size: 16px;
+                text-transform: capitalize;
+                font-weight: 400;
+                
+                &-year {
+                    font-size: 10px;
+                    font-weight: 500;
+                }
+            }
+
+            .car-status {
+                font-size: 10px;
+                text-transform: uppercase;
+                display: flex;
+                gap: 3px;
+                align-items: center;
+                font-weight: 500;
+                height: 20px;
+
+                &::before {
+                    content: '';
+                    width: 5px;
+                    height: 5px;
+                    background: var(--main-blue);
+                    border-radius: 50%;
+                    display: block;
+                }
+
+                &.new-car {
+                    color: var(--main-blue);
+                }
+
+                &.used-car {
+                    color: red;
+
+                    &::before {
+                        background: red;
+                    }
+                }
+            }
+        }
+
+        .car-img-box {
+            width: 100%;
+            position: relative;
+            display: flex;
+            justify-content: center;
+            padding-top: 15px;
+            font-size: 30px;
+
+            .car-bg-name {
+                font-size: 1em;
+                color: var(--main-white);
+                text-transform: uppercase;
+                font-weight: 500;
+                letter-spacing: 5px;
+                position: absolute;
+                top: 0;
+                left: 50%;
+                transform: translateX(-50%);
+                transition: .4s;
+            }
+
+            .car-img {
+                max-width: 223px;
+                width: 100%;
+                object-fit: contain;
+                z-index: 5;
+            }
+        }
 
         .car-descr {
             width: 100%;
@@ -79,6 +193,33 @@ export default {
                 color: var(--main-black);
                 text-transform: uppercase;
                 font-weight: 500;
+                display: flex;
+                align-items: center;
+                gap: 5px;
+            }
+
+            &-icon {
+                max-width: 20px;
+                width: max-content;
+            }
+        }
+
+        &-btn {
+            width: 100%;
+            background: transparent;
+            border-radius: 8px;
+            border: solid 2px var(--main-black);
+            padding: 14.5px 0;
+            font-size: 12px;
+            font-weight: 600;
+            text-transform: uppercase;
+        }
+
+        &:hover {
+            background: var(--main-white);
+
+            .car-bg-name {
+                color: var(--bg-color);
             }
         }
     }
