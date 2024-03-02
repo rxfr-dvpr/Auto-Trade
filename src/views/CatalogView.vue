ed9e82d66674f6e9549c47fd32248a0aab8e0258
Avazbek <saidaliyev.avazbek14@gmail.com>
2024-03-02 12:08:17 +0500

    deals section added to catalog page

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,19 +1,33 @@
 <template>
   <Header/>
+
+  <main class="main">
+    <Deals/>
+  </main>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
+import Deals from '@/components/Deals.vue'
 
 export default {
-    name: 'Catalog Page',
-    components: {
-        Header
-    }
+  name: 'Catalog Page',
+  components: {
+    Header,
+    Deals
+  }
 }
 
 </script>
 
 <style lang="scss" scoped>
+
+.main {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 90px 0;
+  row-gap: 120px;
+}
 
 </style>
